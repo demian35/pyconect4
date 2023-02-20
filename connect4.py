1aa00c4d9108353291ded05a0334d0117fbf9361
Demian <53890358+demian35@users.noreply.github.com>
2023-02-20 01:43:02 -0600

    empezando a modelar el tablero del juego

--- connect4.py
+++ connect4.py
@@ -1,2 +1,15 @@
+#funcion para generar tablero del juego recibe las filas y columas que queremos que tenga el tablero NXM
+#guadaremos bits en una lsita para despues de ahi generar en tablero para el conecta 4
 def creaTablero(N,M):
-    return pass:
+    t=[]
+    for i in range(N):
+        fila=[]
+        for j in range(M):
+            fila.append(0)
+        t.append(fila)
+    return t
+
+if __name__=="__main__":
+    tablero=creaTablero(6,7)
+    print(tablero)
+
